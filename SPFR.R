15281b94ce9b2d3a41bb65614810c455cb1dfb62
irkgreen <irkgreen@gmail.com>
2020-03-25 09:29:45 -0400

    Update SPFR.R
    Updated ver number. Cleaned up user defined values. Fixed error in Standard Deviation calculation. Fixed error in LOSS plot. Added more comments and examples.

--- SPFR.R
+++ SPFR.R
@@ -13,47 +13,69 @@ library(installr) #download using Packages>Install Packages or Tools>Install Pac
 #ALSO Install this: https://cran.r-project.org/bin/windows/Rtools/
 #IMPORTANT: You should make sure that the box is checked to have the installer edit your PATH
 
-#4/21/2017
-VerNum <- c("RCode for SPFs ver 2.2.003")
+#3/25/2020
+VerNum <- c("RCode for SPFs ver 2.2.015")
 
-#Directory
-#Edit this in the event that your data is stored somewhere not relative to your home USER folder
-if (Sys.info()["nodename"] == "MERAK") {
-  MyUserFolder = "D:/SyncFolders"
-} else if(Sys.info()["nodename"] == "COE4249") {
-  MyUserFolder = "C:/Users/clblac2.AD"
-} else {
-  MyUserFolder = dirname(Sys.getenv("HOME"))
-} 
+#################
+# F O L D E R S #
+#################
+#Set folders and files
 
-#define data
+DataFolder = "C:/SPF/"
+InputCSV = "r2u.csv" #name of CSV in DataFolder (include extension)
+CSVpath = paste0(DataFolder,InputCSV)
+OutputProject_Base = "Rural 2 Lane" #Output folder name (will be created in datafolder if it doesn't exist)
+InputData_Base = "Rural 2 Lane 2007-2011" # be sure to uniquely describe the data so it can be traced back to the source
+
+###########
+# D A T A #
+###########
+#define column names that are used below (case sensitive). Many of these are used more than once so it is helpful to define them once here
 TotalColumn = "Total" #The title of the column containing All Crashes. replace spaces with "." e.g. "Total.Crashes"
-KABCColumn = "" #The title of the column containing KABC Crashes. leave blank to ignore
-KABColumn = "KAB" #The title of the column containing KAB Crashes. leave blank to ignore
+KABCColumn = "KABC" #The title of the column containing KABC Crashes. leave blank to ignore
+KABColumn = "KA" #The title of the column containing KAB Crashes. leave blank to ignore
 KAColumn = "" #The title of the column containing KA Crashes. leave blank to ignore
 KColumn = "" #The title of the column containing K Crashes. leave blank to ignore
-AADTColumn = "LASTCNT" #The title of the column containing AADT. 
-LengthColumn = "Length" #The title of the column containing Length. 
-ClassColumn = "Class" #The title of the column containing a class category. leave blank to ignore
+AADTColumn = "aadt" #The title of the column containing AADT. 
+LengthColumn = "Lyears" #The title of the column containing Length. 
+ClassColumn = "" #The title of the column containing a class category (integers). leave blank to ignore
 ClassStart = 1 #Enter a positive integer. This will split the data into groups based on the Class defined above
-ClassEnd = 7 #Enter a positive integer. This will split the data into groups based on the Class defined above
+ClassEnd = 2 #Enter a positive integer. This will split the data into groups based on the Class defined above
 #Path to the input file as a CSV. Must contain headers as defined above.
-CSVpath = paste0(MyUserFolder,"/Dropbox (Trans. Center)/~Annual Projects/HSIP/2016/Cable/Crash Data/OverlayResultsWithClassesClean.csv")
-OutputProject_Base = "Cable class Test" #Project name - created in Documents/R_SPFs
-myFilter_Base = "data[[AADTColumn]] > 0  & data[[LengthColumn]] > 0" # A base filter. Usually best to avoid Length or AADT of 0 or below. 
-InputData_Base = "Median crossover 2011-2015" # be sure to uniquely describe the data so it can be traced back to the source
-initTheta = 1.0 #starting point for theta. adjust as needed
-######################################################################
-######################################################################
-##### Be sure to edit the model form in the glm.nb  if different #####
-######################################################################
-######################################################################
 
-#Examples
-#CSVPath = "D:/SyncFolders/Dropbox (Trans. Center)/PhD/Phd - Segmentation/Attribute/Case 1/SPFs/ParkwayTest/S4_1.csv"
-#myFilter = "data[[AADTColumn]] > 0 & data[[LengthColumn]]"
-#myFilter = "data[[AADTColumn]] < 500 & data$SHLDWID == 2 & data$LANEWID == 9 & (data$CURVECLS == 'A' | data$CURVECLS == 'B') & data$MEDTYPE == 8 & (data$GRADECLS == 'A' | data$GRADECLS == '')"
+#################
+# F I L T E R S #
+#################
+# each filter line requires a filter and a description. Be sure to update both as the description will be included in the output folder name
+#Examples (you can use the variable references data[[LengthColumn]] or directly like data$LANEWID)
+myFilter_Base = "data[[LengthColumn]] > 0"# A base filter. Usually best to avoid Length or AADT of 0 or below. 
+#myFilter_Base = paste0(myFilter_Base," & data$LANEWID == 9"); OutputProject_Base = paste0(OutputProject_Base," LW9")
+#myFilter_Base = paste0(myFilter_Base," & data$SHLDWID == 3"); OutputProject_Base = paste0(OutputProject_Base," SW3")
+#myFilter_Base = paste0(myFilter_Base," & data$IntsctID_1 == 0"); OutputProject_Base = paste0(OutputProject_Base," noInt")
+#myFilter_Base = paste0(myFilter_Base," & (data$GRADECLS == 'A' | data$GRADECLS == '')"); OutputProject_Base = paste0(OutputProject_Base," noVC")
+#myFilter_Base = paste0(myFilter_Base," & (data$CURVECLS == 'A' | data$CURVECLS == 'B')"); OutputProject_Base = paste0(OutputProject_Base," noHC")
+#myFilter_Base = paste0(myFilter_Base," & data$MEDTYPE == 8"); OutputProject_Base = paste0(OutputProject_Base," noMed")
+#myFilter_Base = paste0(myFilter_Base," & data[[AADTColumn]] < 500"); OutputProject_Base = paste0(OutputProject_Base," ADT500")
+#myFilter_Base = paste0(myFilter_Base," & data$RT_UNIQUE != '030-US-0060  -000'"); OutputProject_Base = paste0(OutputProject_Base," noUS60")
+#myFilter_Base = paste0(myFilter_Base," & data[[AADTColumn]] > 0"); OutputProject_Base = paste0(OutputProject_Base," ADTgtzero")
+#myFilter_Base = paste0(myFilter_Base," & data[[AADTColumn]] < 2000"); OutputProject_Base = paste0(OutputProject_Base," ADT2000")
+#myFilter_Base = paste0(myFilter_Base," & data[[AADTColumn]] < 2500"); OutputProject_Base = paste0(OutputProject_Base," ADT2500")
+#myFilter_Base = paste0(myFilter_Base," & data$LANEWID >= 9 & data$LANEWID <= 10"); OutputProject_Base = paste0(OutputProject_Base," LW9-10")
+#myFilter_Base = paste0(myFilter_Base," & data$LANEWID >= 8 & data$LANEWID <= 10"); OutputProject_Base = paste0(OutputProject_Base," LW8-10")
+#myFilter_Base = paste0(myFilter_Base," & data$SHLDWID >= 2 & data$SHLDWID <= 3"); OutputProject_Base = paste0(OutputProject_Base," SW2-3")
+#myFilter_Base = paste0(myFilter_Base," & data$SHLDWID >= 2 & data$SHLDWID <= 4"); OutputProject_Base = paste0(OutputProject_Base," SW2-4")
+#myFilter_Base = paste0(myFilter_Base," & data$SPEEDLIM >= 50"); OutputProject_Base = paste0(OutputProject_Base," SL50p")
 
+###########
+# M I S C #
+###########
+initTheta = 0.1 #starting point for theta. adjust as needed
+
+######################################################################
+######################################################################
+##### Be sure to edit the model form in the glm.nb if different ######
+######################################################################
+######################################################################
 #flag to test if data is bound
 databind = FALSE
 
@@ -103,15 +125,24 @@ RunSPF <- function() {
   crash=data2[[CrashColumn]]
   lnADT=log(data2[[AADTColumn]])
   lnL=log(data2[[LengthColumn]])
+  #ru=data2$ru #refer to a column directly
+  
   #Calculate length if it doesn't exists - this will make zero length filter difficult
   #lnL=log(EMP-BMP)
   
   init.theta = initTheta
-  
-#################################################################
+
+  ###########################################################################################
+  ###########################################################################################  
+  # Be sure to edit the datametrics list below to reflect any added parameters to the model #
+  ###########################################################################################
+  ###########################################################################################
   SPF=glm.nb(crash~lnADT+offset(lnL))
-#################################################################
-  
+  #SPF=glm.nb(crash~lnADT+ru+offset(lnL)) # added a variable, see the users guide for more
+  ###########################################################################################
+  ###########################################################################################
+  ###########################################################################################    
+
   #add results from GLM
   dataout <- cbind(data2,Predicted=SPF$fitted.values,Residuals=resid(SPF,type="resp"),CumulRes=cumsum(resid(SPF,type="resp")))
   
@@ -166,8 +197,7 @@ RunSPF <- function() {
   jpeg(file=paste0(OutPath,OutputProject,"_AADTBox.png"))
     boxplot(dataout[[AADTColumn]], main="AADT Box Plot", ylab="AADT")
   dev.off()
-  
-   
+
   #Metrics
   Sample = nrow(dataout)
   Mileage = sum(dataout[[LengthColumn]])
@@ -179,10 +209,24 @@ RunSPF <- function() {
   CDP = sum(datalimits$Per_CURE)/length(datalimits$Per_CURE)*100
   MACD = max(abs(datalimits$CumulRes))  
   MAD = mean(abs(dataout$Residuals))
-  datametrics <- data.frame(Values = c(Sample,Mileage,Crashes,RSquared,CDP,MACD,MAD,SPF$theta,coef(summary(SPF))["(Intercept)","Estimate"],coef(summary(SPF))["lnADT","Estimate"], SPF$SE.theta, SPF$aic, "", "", ""))
+
+  ############################################################################################################
+  ############################################################################################################
+  # add or remove items to all three lists to correspond to any model parameters (see user guide for examples)
+  ############################################################################################################
+  ############################################################################################################
+  datametrics <- data.frame(Values = c(Sample,Mileage,Crashes,RSquared,CDP,MACD,MAD,SPF$theta,coef(summary(SPF))["(Intercept)","Estimate"],coef(summary(SPF))["lnADT","Estimate"],SPF$SE.theta, SPF$aic, "", "", ""))
   datametrics$Notes <- c("100-200 intersections*","100-200 miles*","300 crashes per year*","Higher values preferred","Less than 5%","Smaller values preferred","Smaller values preferred","Higher values preferred",paste0("(Intercept) zvalue= ",coef(summary(SPF))["(Intercept)","Pr(>|z|)"]) ,paste0("lnADT zvalue= ",coef(summary(SPF))["lnADT","Pr(>|z|)"]),"", "", myFilter, InputData,"*As recommended by FHWA-SA-14-004") 
   attr(datametrics, "row.names") <- c("Sample","Length","Crashes","R2","CDP","MACD","MAD","Theta","Alpha","Beta","StdErr","AIC", "Filter","Input Data","")
   datametrics$Values = as.numeric(as.character(datametrics$Values))
+  ############################################################################################################
+  ############################################################################################################
+  ### Example with variable added#############################################################################
+  #datametrics <- data.frame(Values = c(Sample,Mileage,Crashes,RSquared,CDP,MACD,MAD,SPF$theta,coef(summary(SPF))["(Intercept)","Estimate"],coef(summary(SPF))["lnADT","Estimate"],coef(summary(SPF))["ru","Estimate"],SPF$SE.theta, SPF$aic, "", "", ""))
+  #datametrics$Notes <- c("100-200 intersections*","100-200 miles*","300 crashes per year*","Higher values preferred","Less than 5%","Smaller values preferred","Smaller values preferred","Higher values preferred",paste0("(Intercept) zvalue= ",coef(summary(SPF))["(Intercept)","Pr(>|z|)"]) ,paste0("lnADT zvalue= ",coef(summary(SPF))["lnADT","Pr(>|z|)"]),paste0("ru zvalue= ",coef(summary(SPF))["ru","Pr(>|z|)"]),"", "", myFilter, InputData,"*As recommended by FHWA-SA-14-004") 
+  #attr(datametrics, "row.names") <- c("Sample","Length","Crashes","R2","CDP","MACD","MAD","Theta","Alpha","BetaAADT","BetaRU","StdErr","AIC", "Filter","Input Data","")
+  #datametrics$Values = as.numeric(as.character(datametrics$Values))
+  ############################################################################################################
   
   #Stats
   statTitles <- data.frame(Stat=c("Mean","StdDev","Min","Q1","Median","Q3","Max"))
@@ -206,17 +250,17 @@ RunSPF <- function() {
   dataout$EB_Estimate <- dataout[[CrashColumn]]*(1-dataout$Weight) + dataout$Predicted*(dataout$Weight)
   dataout["PCR"] <- NA
   dataout$PCR <- dataout$EB_Estimate - dataout$Predicted
-  dataout["StdError"] <- NA
-  dataout$StdError <- sqrt((1-dataout$Weight)*dataout$EB_Estimate)
+  dataout["SDev"] <- NA
+  dataout$SDev <- sqrt((1-dataout$Weight)*dataout$Predicted) 
   
     #calculate data for LOSS Plot
-  dataout=cbind(dataout,Expected=dataout$Predicted/dataout$Length,LOSS_Crashes=dataout$EB_Estimate)
+  dataout<-cbind(dataout,Expected=dataout$Predicted/dataout[[LengthColumn]],LOSS_Crashes=dataout$EB_Estimate)
   dataout=cbind(dataout,beta=dataout$Expected/SPF$theta) # Fixed Error
   dataout=cbind(dataout,lowloss=qgamma(0.05,SPF$theta,scale=dataout$beta),uploss=qgamma(0.95,SPF$theta,scale=dataout$beta))
   dataout=cbind(dataout,LOSS_score=ifelse(dataout$LOSS_Crashes>dataout$uploss,4,ifelse(dataout$LOSS_Crashes>dataout$Expected,3,ifelse(dataout$LOSS_Crashes>dataout$lowloss,2,1)))) # fixed error
   
   #create LOSS plot
-  LOSSPlot <- ggplot(dataout, aes(dataout$AADT, y = value, color = variable)) + 
+  LOSSPlot <- ggplot(dataout, aes(dataout[[AADTColumn]], y = value, color = variable)) + 
     geom_point(aes(y = uploss, col = "Upper LOSS")) + 
     geom_point(aes(y = lowloss, col = "Lower LOSS")) + 
     geom_point(aes(y = Expected, col = "Expected")) + 
@@ -265,9 +309,9 @@ if (databind) {
       InputData = paste0(ClassOut,InputData_Base)
       OutputProject = paste0(OutputProject_Base,ClassOut)
       #create folders
-      dir.create(file.path(Sys.getenv("HOME"), "R_SPFs"))
-      dir.create(file.path(paste0(Sys.getenv("HOME"),"/R_SPFs"),OutputProject))
-      OutPath = paste0(Sys.getenv("HOME"),"/R_SPFs/",OutputProject,"/")
+      dir.create(file.path(DataFolder, "R_SPFs"))
+      dir.create(file.path(paste0(DataFolder,"/R_SPFs"),OutputProject))
+      OutPath = paste0(DataFolder,"/R_SPFs/",OutputProject,"/")
       
       RunSPF()
       print(paste0("All crashes finished",ClassOut))
@@ -278,8 +322,8 @@ if (databind) {
         InputData = paste0(InputData_Base," - KABC",ClassOut)
         OutputProject = paste0(OutputProject_Base," - KABC",ClassOut)
         #create folders
-        dir.create(file.path(paste0(Sys.getenv("HOME"),"/R_SPFs"), OutputProject))
-        OutPath = paste0(Sys.getenv("HOME"),"/R_SPFs/",OutputProject,"/")
+        dir.create(file.path(paste0(DataFolder,"/R_SPFs"), OutputProject))
+        OutPath = paste0(DataFolder,"/R_SPFs/",OutputProject,"/")
         RunSPF()
         print(paste0("KABC crashes finished",ClassOut))
       }
@@ -290,8 +334,8 @@ if (databind) {
         InputData = paste0(InputData_Base," - KAB",ClassOut)
         OutputProject = paste0(OutputProject_Base," - KAB",ClassOut)
         #create folders
-        dir.create(file.path(paste0(Sys.getenv("HOME"),"/R_SPFs"), OutputProject))
-        OutPath = paste0(Sys.getenv("HOME"),"/R_SPFs/",OutputProject,"/")
+        dir.create(file.path(paste0(DataFolder,"/R_SPFs"), OutputProject))
+        OutPath = paste0(DataFolder,"/R_SPFs/",OutputProject,"/")
         RunSPF()
         print(paste0("KAB crashes finished",ClassOut))
       }
@@ -302,8 +346,8 @@ if (databind) {
         InputData = paste0(InputData_Base," - KA",ClassOut)
         OutputProject = paste0(OutputProject_Base," - KA",ClassOut)
         #create folders
-        dir.create(file.path(paste0(Sys.getenv("HOME"),"/R_SPFs"), OutputProject))
-        OutPath = paste0(Sys.getenv("HOME"),"/R_SPFs/",OutputProject,"/")
+        dir.create(file.path(paste0(DataFolder,"/R_SPFs"), OutputProject))
+        OutPath = paste0(DataFolder,"/R_SPFs/",OutputProject,"/")
         RunSPF()
         print(paste0("KA crashes finished",ClassOut))
       }
@@ -313,8 +357,8 @@ if (databind) {
         InputData = paste0(InputData_Base," - K",ClassOut)
         OutputProject = paste0(OutputProject_Base," - K",ClassOut)
         #create folders
-        dir.create(file.path(paste0(Sys.getenv("HOME"),"/R_SPFs"), OutputProject))
-        OutPath = paste0(Sys.getenv("HOME"),"/R_SPFs/",OutputProject,"/")
+        dir.create(file.path(paste0(DataFolder,"/R_SPFs"), OutputProject))
+        OutPath = paste0(DataFolder,"/R_SPFs/",OutputProject,"/")
         RunSPF()
         print(paste0("K crashes finished",ClassOut))
       }
